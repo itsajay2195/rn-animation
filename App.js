0c31c9b3aedfc1a8fae389db8352299799de8a93
itsajay2195 <ajayqae95@gmail.com>
2022-04-27 22:21:05 +0530

    carousel animation using animated api

--- App.js
+++ App.js
@@ -2,10 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView, Image,Animated } from 'react-native';
 import {useState} from 'react';
 
-const HEADER_MAX_HEIGHT = 120
-const HEADER_MIN_HEIGHT = 70
-const PROFILE_IMAGE_MAX_HEIGHT =80
-const PROFILE_IMAGE_MIN_HEIGHT =40
 
 export default function App() {
   
