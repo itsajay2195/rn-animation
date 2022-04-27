8caf2988edc6bcaaec809996867c7f3ce18ca4a4
itsajay2195 <ajayqae95@gmail.com>
2022-04-27 23:03:23 +0530

    implemented the position indicator

--- components/Carousel.js
+++ components/Carousel.js
@@ -34,27 +34,59 @@ const DATA = [
     "image": "https://cdn-icons-png.flaticon.com/512/3571/3571665.png"
   }
 ]
+
+const Indicator = ({scrollX})=>{
+    return(
+        <View style={{position:'absolute',bottom:100,flexDirection:'row'}}>
+           {DATA.map((_,i)=>{
+               return(
+                   <View key={'indicator'+i} 
+                     style={{margin:10,height:10,width:10,borderRadius:5,backgroundColor:'#333'}}>
+
+                   </View>
+               )
+           })}
+        </View>
+    )
+}
+
 const Carousel = () => {
+    const scrollX = React.useRef(new Animated.Value(0)).current
   return (
     <View style={styles.container} >
       <Text>hi</Text>
       <Animated.FlatList
-        data={DATA}
+         pagingEnabled={true}
+        data={DATA} 
         keyExtractor={item=>item.key}
         horizontal
+        contentContainerStyle={{paddingBottom:100}}
+        scrollEventThrottle={32}
+        onScroll={Animated.event(
+            [{nativeEvent:{contentoffset:{x:scrollX}}}],
+            {useNativeDriver:false}
+        )}
         showsHorizontalScrollIndicator={false}
         renderItem={({item,index})=>{
             return(
-                <View style={{width,justifyContent:'center',alignItems:'center'}}>
-                    <Image
-                        source={{uri:item.image}}
-                        style={{width:width/2,height:width/2,resizeMode:'contain'}}
-                    />
+                <View style={{width,alignItems:'center',padding:20}}>
+                    <View style={{flex:0.7,justifyContent:'center'}}>
+                        <Image
+                            source={{uri:item.image}}
+                            style={{width:width/2,height:width/2,resizeMode:'contain'}}
+                        />
+                    </View >
+
+                    <View style={{flex:0.3}}>
+                         <Text style={{fontWeight:'800',fontSize:24,marginBottom:10}}>{item.title}</Text>
+                         <Text style={{fontWeight:'300',}}>{item.description}</Text>
+                    </View>
                 </View>
             )
         }}
 
       />
+      <Indicator  />
     </View>
   )
 }
